f145ecafb622843e4f0359d9c622b6f2e1151312
Radley Sustaire <radleygh@gmail.com>
2026-04-29 14:21:58 -0700

    Deploy: Roguelike m234, Roguelike III m4, Spellroads m33, Astral Crypts m30, Wanderlight m14, Vagrant & Ash m80, Aethermoor Chronicles m9, Stormtown m19, Penny Panic m29, Emberveil m378, HOOPSHOT m4, Stable Construct m20, Neonaut m15 [2026-04-29 14:21]

--- game13/assets/ConfirmModal-BWqHYjcw.js
+++ game13/assets/ConfirmModal-BWqHYjcw.js
@@ -1,4 +1,4 @@
-import{i as m,c as l,r as f}from"./play-Dp9CKpgY.js";import"./savesClient-DUFjgBxb.js";const s=`
+import{i as m,c as l,r as f}from"./play-kynXtyPf.js";import"./savesClient-DUFjgBxb.js";const s=`
 .confirm-modal-overlay {
   position: fixed; inset: 0; background: rgba(0,0,0,0.72);
   z-index: 5000; display: flex; align-items: center; justify-content: center;
